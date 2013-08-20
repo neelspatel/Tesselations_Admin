be7ac43aee938b44433b482f38e8d79d37311b83
Neel Patel <npatel@college.harvard.edu>
2013-08-20 19:24:43 -0400

    Working on forms still

--- mysite/data/models.py
+++ mysite/data/models.py
@@ -31,7 +31,7 @@ class JQueryUIDatepickerWidget(forms.DateInput):
 
 class AMA1(models.Model):
 	grievance_received_by = models.TextField(null=True, blank=True)
-	date_of_recording = forms.DateField(required=False, widget=JQueryUIDatepickerWidget)
+	date_of_recording = models.DateTimeField(widget=JQueryUIDatepickerWidget)
 	name_and_id_of_person_with_grievance = models.TextField(null=True, blank=True)
 	contact_details = models.TextField(null=True, blank=True)
 	location = models.TextField(null=True, blank=True)
