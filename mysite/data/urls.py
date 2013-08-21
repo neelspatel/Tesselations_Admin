e5dd77e5bb032db29b450704db68dc6ba385e078
Neel Patel <npatel@college.harvard.edu>
2013-08-21 00:18:45 -0400

    trying to stop url catch all

--- mysite/data/urls.py
+++ mysite/data/urls.py
@@ -3,8 +3,8 @@ from django.conf import settings
 from data import views
 
 urlpatterns = patterns('',
-    url(r'^AMA1/', views.main, name = 'AMA1'),
-    url(r'^AMA1/save/', views.saveAMA1, name = 'AMA1'),
-    url(r'^AMA1/new/', views.newAMA1, name = 'AMA1'),
+    url(r'^AMA1/$', views.main, name = 'AMA1'),
+    url(r'^AMA1/save/$', views.saveAMA1, name = 'AMA1'),
+    url(r'^AMA1/new/$', views.newAMA1, name = 'AMA1'),
     url(r'^AMA1/(?P<data_id>\w+)/$', views.existingAMA1, name='existingAMA1'),
 )
