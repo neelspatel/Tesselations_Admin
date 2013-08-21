c5f50014b3c8016a706ab4e01102db23465a56b3
Neel Patel <npatel@college.harvard.edu>
2013-08-21 11:01:22 -0400

    displaying results

--- mysite/data/views.py
+++ mysite/data/views.py
@@ -6,6 +6,9 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from data.models import AMA1, AMA1Form
 import json
+from django.core import serializers
+from time import gmtime, strftime
+
 
 def main(request):	
 
@@ -41,3 +44,33 @@ def existingAMA1(request, data_id):
 	current = AMA1.objects.get(id=data_id)
 	form = AMA1Form(instance = current)
 	return render(request, 'data/AMA1.html', {'form': form, 'has_existing_id': data_id})
+
+def csv(request):
+	response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="AMA1_' + strftime("%Y-%m-%d %H.%M.%S", gmtime()) + '.csv"'
+
+    writer = csv.writer(response)
+
+	serialized = serializers.serialize("python", AMA1.objects.all())
+
+	keys = serialized[0]["fields"].keys()	
+	writer.writerow(["id"] + keys)
+
+	#header = "id"
+	#for key in keys:		
+	#	header = header + "," + key
+	
+	for item in serialized:
+		row = []
+		row.append(str(item['pk']))
+
+		for key in keys:
+			value = str(item['fields'][key])
+			value = value.replace('"', "'")
+			row.append('"' + value + '"')
+
+		writer.writerow(row)
+
+	return response
+
+
