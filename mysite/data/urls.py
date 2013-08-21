e40f7ce4dba5d916f55770d05e9ac9dd3572be20
Neel Patel <npatel@college.harvard.edu>
2013-08-21 01:02:51 -0400

    displaying results

--- mysite/data/urls.py
+++ mysite/data/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
     url(r'^AMA1/$', views.main, name = 'AMA1'),
     url(r'^AMA1/save/$', views.saveAMA1, name = 'AMA1'),
     url(r'^AMA1/new/$', views.newAMA1, name = 'AMA1'),
-    url(r'^AMA1/(?P<data_id>\w+)/$', views.existingAMA1, name='existingAMA1'),
     url(r'^AMA1/list/$', views.list, name = 'list'),
+    url(r'^AMA1/(?P<data_id>\w+)/$', views.existingAMA1, name='existingAMA1'),
 )
